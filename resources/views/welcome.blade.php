ac4375fc4668419e9fd3c79a34f11e41380eb51a
Anthony DeVenuto <adevenuto@macbook-m1.local>
2023-05-28 19:55:04 -0500

    removed react

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,16 +10,15 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-        @vite('resources/css/app.css')
-
-        
+        {{-- @viteReactRefresh
+        @vite(['resources/js/app.js', 'resources/css/app.css']) --}}
 
         
     </head>
     <body class="antialiased">
-        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+        <div class="relative min-h-screen bg-gray-100 bg-center sm:flex sm:justify-center sm:items-center bg-dots-darker dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
-                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                <div class="z-10 p-6 text-right sm:fixed sm:top-0 sm:right-0">
                     @auth
                         <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
                     @else
@@ -31,9 +30,8 @@
                     @endauth
                 </div>
             @endif
-
-            <h1 class="text-white">GOLF TOURNEY</h1>
-            
         </div>
+
+        
     </body>
 </html>
